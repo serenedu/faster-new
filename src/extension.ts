fe53f5257f7ebc87398496dd3dfc05a94a222235
Wenfang Du <duwenfangcn@163.com>
2021-03-13 10:26:49 +0800

    release: v1.1.20
    added deactivate function

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'
+import { displayName } from '../package.json'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate() {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Your extension "Quick New File & Folder" is now active.')
+  console.log(`Extension "${displayName}" is activated.`)
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+  console.log(`Extension "${displayName}" is deactivated.`)
 }
